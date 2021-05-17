bb6907f137245bf2371783490586b0f3591e91a2
CarolKuch <kar.kucharska@gmail.com>
2021-05-17 13:20:58 +0200

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddPostComponent } from './add-post/add-post.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { PostsComponent } from './posts/posts.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'posts', component: PostsComponent },
+  { path: 'add', component: AddPostComponent },
+  { path: '', redirectTo: '/posts', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostsComponent } from './posts/posts.component';
+import { PostComponent } from './posts/post/post.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { AddPostComponent } from './add-post/add-post.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostsComponent,
+    PostComponent,
+    PageNotFoundComponent,
+    NavigationComponent,
+    AddPostComponent
   ],
   imports: [
     BrowserModule,
